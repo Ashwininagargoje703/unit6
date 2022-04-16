28824543d91ed512e07346f4279dcdb7879f075c
Ashwini Nagargoje <ashwininagargoje703@gmail.com>
2022-04-17 01:58:51 +0530

    done

--- react/src/Redux/store.jsx
+++ react/src/Redux/store.jsx
@@ -5,7 +5,7 @@ import thunk from "redux-thunk";
 const composeEnhancers =
   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-     
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
       })
     : compose;
 
@@ -13,7 +13,7 @@ const middleware = [thunk];
 
 const enhancer = composeEnhancers(
   applyMiddleware(...middleware),
- 
+  // other store enhancers if any
 );
 
 const rootReducer = combineReducers({
